597f4f933b584579e63f11a32591631eeed867bc
DanteBonino <43711055+DanteBonino@users.noreply.github.com>
2023-06-04 11:16:46 -0300

    Punto 1

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,24 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib () where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+--Punto 1:
+estaSaludable :: Persona -> Bool
+estaSaludable unaPersona = (not . esObesa) unaPersona && ((>5). tonificacion) unaPersona
+
+esObesa :: Persona -> Bool
+esObesa = ((> 100) . peso)
+
+peso :: Persona -> Float
+peso (_, peso, _) = peso
+
+tonificacion :: Persona -> Int
+tonificacion (_,_, tonificacion) = tonificacion
+
+edad :: Persona -> Int
+edad (edad, _ ,_) = edad
+
+type Persona = (Int, Float, Int)
+--También se podría hacer directamente por pattern matching
+
+--Punto 2:
+type Calorias = Int
+bajarDePeso :: Calorias

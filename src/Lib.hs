52d5e4a650376e74e934fb65fd21a5c4b2bbfbbe
DanteBonino <43711055+DanteBonino@users.noreply.github.com>
2023-06-04 11:41:56 -0300

    Punto 2
    Se Hicieron 2 alternativas una más clara, pero que repite más código. La otra, menos clara, pero repite menos código.
    Se cambió la edad de las personas de Int a Float pq no cambia en nada en futuros puntos y facilita las operaciones en el punto 2

--- src/Lib.hs
+++ src/Lib.hs
@@ -13,12 +13,37 @@ peso (_, peso, _) = peso
 tonificacion :: Persona -> Int
 tonificacion (_,_, tonificacion) = tonificacion
 
-edad :: Persona -> Int
+edad :: Persona -> Float
 edad (edad, _ ,_) = edad
 
-type Persona = (Int, Float, Int)
+type Persona = (Float, Float, Int)
 --También se podría hacer directamente por pattern matching
 
 --Punto 2:
-type Calorias = Int
-bajarDePeso :: Calorias
+type Calorias = Float
+
+bajarDePeso :: Calorias -> Persona -> Persona
+bajarDePeso unasCalorias unaPersona
+  | esObesa unaPersona                             = reducirPeso (unasCalorias / 150) unaPersona
+  | tieneMasDe 30 unaPersona && unasCalorias > 200 = reducirPeso 1 unaPersona
+  | otherwise                                      = reducirPeso (unasCalorias / ((peso unaPersona) * (edad unaPersona)) ) unaPersona
+
+tieneMasDe :: Float -> Persona -> Bool
+tieneMasDe unaEdad = ((> unaEdad) . edad)
+
+reducirPeso :: Float -> Persona -> Persona
+reducirPeso unaCantidad = modificarPeso (subtract unaCantidad)
+
+modificarPeso :: (Float -> Float) -> Persona -> Persona
+modificarPeso modificacion (unaEdad, unPeso, unaTonificacion) = (unaEdad, modificacion unPeso, unaTonificacion)
+
+--También se podría hacer así (Me parece menos clara, pero se repite menos código)
+
+bajarDePesoV2 :: Calorias -> Persona -> Persona
+bajarDePesoV2 unasCalorias unaPersona = (flip reducirPeso unaPersona . calcularPesoSegunCalorias unasCalorias) unaPersona
+
+calcularPesoSegunCalorias :: Calorias -> Persona -> Float
+calcularPesoSegunCalorias unasCalorias unaPersona
+  | esObesa unaPersona                             = (unasCalorias / 150)
+  | tieneMasDe 30 unaPersona && unasCalorias > 200 =  1
+  | otherwise                                      = (unasCalorias / (peso unaPersona) * (edad unaPersona))
